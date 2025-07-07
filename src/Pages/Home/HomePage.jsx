ec754b844847801677102fec7cd8eba8487cf34b
emaduddin678 <emaduddin678@gmail.com>
2025-07-08 00:17:14 +0600

    done basic

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -11,41 +11,90 @@ const HomePage = () => {
             src="/GP_ONE_Logo2.png"
             alt="gp one logo"
           />
-          <div className="CountDown text-center ">
-            <h2>Event goes live in</h2>
+          <div className="CountDown text-center pt-6">
+            <h2 className="text-3xl py-2">Event goes live in</h2>
             <div className="countdown bg-black">{/* <Countdown /> */}</div>
-            <div className="Watch">
-              <h3>Watch live on</h3>
-              <img src="" alt="" className="yt" />
-              <img src="" alt="" className="yt" />
-              {/* <!-- From Uiverse.io by andrew-demchenk0 -->  */}
-              <button>
-                <span class="icon">
-                  <svg
-                    fill="none"
-                    height="33"
-                    viewBox="0 0 120 120"
-                    width="33"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="m120 60c0 33.1371-26.8629 60-60 60s-60-26.8629-60-60 26.8629-60 60-60 60 26.8629 60 60z"
-                      fill="#cd201f"
-                    ></path>
-                    <path
-                      d="m25 49c0-7.732 6.268-14 14-14h42c7.732 0 14 6.268 14 14v22c0 7.732-6.268 14-14 14h-42c-7.732 0-14-6.268-14-14z"
-                      fill="#fff"
-                    ></path>
-                    <path
-                      d="m74 59.5-21 10.8253v-21.6506z"
-                      fill="#cd201f"
-                    ></path>
-                  </svg>
-                </span>
-                <span class="text1">Follow me</span>
-                <span class="text2">1,2k</span>
-              </button>
+            <div className="Watch  flex justify-center ">
+              <div className="px-10 py-2 bg-white ring-1 ring-cyan-800 rounded-4xl flex justify-center items-center">
+                <h3 className="mr-4 font-medium">Watch live on</h3>
+                <button>
+                  <img src="/youtube-logo-hd.png" alt="youtube-logo-hd.png" />
+                </button>
+                <button>
+                  <img src="/Facebook-logo.png" alt="Facebook-logo.png" />
+                </button>
+              </div>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className="bg-[url('/section2.jpg')] bg-cover bg-top py-40 flex flex-col items-center justify-center">
+        <div className="info">
+          <h2 className="text-5xl text-center font-bold mb-4">
+            Beyond connection, in One Ecosystem
+          </h2>
+          <p className="text-3xl max-w-2xl mx-auto text-center leading-12">
+            Discover ideas, experiences and solutions that push the boundaries
+            of what’s possible. Join us as innovation takes a new flight.
+          </p>
+        </div>
+        <div className="events flex gap-20 font-bold text-2xl pt-32">
+          {/* Gradient border layer */}
+          {/* <div className="relative rounded-3xl  eventCard bg-white flex flex-col items-center rounded-3xl z-10">
+            <div className="absolute inset-0 rounded-3xl p-[2px] bg-gradient-to-tr from-gray-400 to-transparent z-[-1]"></div>
+            <div className="bg-white w-full h-full px-20 py-16">
+              <h3 className="mb-6 font-medium">Event Keynote</h3>
+              <button className="btn-grad text-lg">Stay Tuned</button>
+            </div>
+          </div> */}
+
+          <div className="bg-gradient-to-tr from-gray-200 to-transparent bg-white flex flex-col items-center p-1 rounded-3xl">
+            <div className="eventCard bg-white flex flex-col items-center px-20 py-16 rounded-3xl">
+              <h3 className="mb-6 font-medium">Event Keynote</h3>
+              <button className="btn-grad text-lg ">Stay Tuned</button>
+            </div>
+          </div>
+          <div className="bg-gradient-to-tr from-gray-12`00 to-transparent bg-white flex flex-col items-center p-1 rounded-3xl">
+            <div className="eventCard bg-white flex flex-col items-center px-20 py-16 rounded-3xl">
+              <h3 className="mb-6 font-medium">New Plaltforms</h3>
+              <button className="btn-grad text-lg ">Stay Tuned</button>
+            </div>
+          </div>
+        </div>
+        <div className="events flex justify-center items-center w-full max-w-1/2 gap-10 pt-20">
+          <img
+            className="flex-1 w-1/2"
+            src="/GP_ONE_Logo.png"
+            alt="GP_ONE_Logo"
+          />
+          <img
+            className="flex-1 w-1/4 p-10"
+            src="/grameenphone_logo.png"
+            alt="grameenphone_logo"
+          />
+        </div>
+        <div className="events flex justify-center items-center w-full max-w-1/2 gap-10 pt-20">
+          <div class="box">
+            <button class="visitbutton">Visit Now</button>
+            {/* <div class="snowfall">
+            <span style={{-i: 12}} className="snowflake" ></span>
+          <span style={{-i: 31}} className="snowflake" ></span>
+          <span style={{-i: 18}} className="snowflake" ></span>
+          <span style={{-i: 9}} className="snowflake" ></span>
+          <span style={{-i: 41}} className="snowflake" ></span>
+          <span style={{-i: 22}} className="snowflake" ></span>
+          <span style={{-i: 16}} className="snowflake" ></span>
+          <span style={{-i: 35}} className="snowflake" ></span>
+          <span style={{-i: 9}} className="snowflake" ></span>
+          <span style={{-i: 14}} className="snowflake" ></span>
+          <span style={{-i: 19}} className="snowflake" ></span>
+          <span style={{-i: 6}} className="snowflake" ></span>
+          <span style={{-i: 53}} className="snowflake" ></span>
+          <span style={{-i: 26}} className="snowflake" ></span>
+          <span style={{-i: 34}} className="snowflake" ></span>
+          <span style={{-i: 18}} className="snowflake" ></span>
+          <span style={{-i: 8}} className="snowflake" ></span>
+            </div> */}
           </div>
         </div>
       </section>
